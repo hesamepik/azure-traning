87f475617151e8318598091b1397bbe6fc941ba9
Hesam Epik <hesamepik@gmail.com>
2025-11-19 11:29:01 +0330

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.json({ message: "CI/CD is working!" });
+});
+
+app.listen(3000, () => {
+	console.log("Server running on port 3000");
+});
